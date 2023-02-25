028ae9e88b16e31a13ec55ac9b4a8c239688ce75
addeguzman2 <119831458+addeguzman2@users.noreply.github.com>
2023-02-25 20:00:53 +0800

    fixed variable name

--- week9/upcoming.php
+++ week9/upcoming.php
@@ -60,7 +60,7 @@ body {
          $password = "b3ntRhino98";
          $dbname = "webprogmi212";
 
-        $connection = new mysqli($servername, $username, $password, $database);
+        $connection = new mysqli($servername, $username, $password, $dbname);
 
         if ($connection->connect_error) {
             die("Connection failed: " . $connection->connect_error);
