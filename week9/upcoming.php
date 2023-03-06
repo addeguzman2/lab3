f565a3ca5c99199bfce68fab3adc603ff82f08d5
addeguzman2 <119831458+addeguzman2@users.noreply.github.com>
2023-03-07 02:06:41 +0800

    fixed db path

--- week9/upcoming.php
+++ week9/upcoming.php
@@ -55,10 +55,10 @@ body {
 
     <tbody>
         <?php
-          $servername = "localhost";
-          $username = "root";
-          $password = "";
-          $dbname = "sampledb";
+          $servername = "192.168.150.213";
+          $username = "webprogmi212";
+          $password = "b3ntRhino98";
+          $dbname = "webprogmi212";
 
         $connection = new mysqli($servername, $username, $password, $dbname);
 
